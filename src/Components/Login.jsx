23d4e9488a00569fa45cb2144433be3ed384fb22
Mehetaj <121656128+Mehetaj@users.noreply.github.com>
2023-05-01 22:03:33 +0600

    google and github design added

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { FaGithub, FaGoogle } from 'react-icons/fa';
 
 const Login = () => {
     return (
         <div className='container mx-auto flex justify-center mt-20 items-center max-w-full'>
-           
-            <form className='login-container border p-10'>
+
+            <div>
+                <form className='login-container border p-10'>
                     <h2 className='text-xl font-bold'>Login</h2>
 
                     <div className="relative z-0 w-full mt-[54px] mb-[55px] group">
@@ -20,12 +22,38 @@ const Login = () => {
                         <span><input type="checkbox" name="" value="Remember Me" id="" /><span className='font-bold ml-2'>Remember Me</span></span>
                         <Link className=' text-red-500 underline'>Forgot Password</Link>
                     </div>
-                    <input className='submit bg-rose-400 px-5 py-2 text-lg text-white rounded-lg' type="submit" value="Login" />
-                    <div className='w-[290px] mt-2 font-bold text-center mx-auto'>
-                        
+                    <input className='submit w-full mt-4 bg-rose-400 px-5 py-2 text-lg text-white rounded-lg' type="submit" value="Login" />
+                    <div className=' mt-2 font-bold text-center mx-auto'>
+                        <p>Already Have an Account? <Link className='underline text-red-400'>Sign Up Here</Link></p>
                     </div>
                 </form>
-            
+
+                <div className='mt-4 gap-2 flex items-center'>
+                    <div className='w-[200px] left-[506px] top-[663px] border'></div>
+                    <div>Or</div>
+                    <div className='w-[200px] left-[506px] top-[663px] border'></div>
+                </div>
+
+                <div>
+                    <div className='flex justify-center items-center mt-5'>
+                        <div>
+                            <Link>
+                                <div className='popup-login border px-10 py-3 rounded-xl flex items-center'>
+                                    <FaGithub className='w-[37px] h-[37px] ml-2 rounded-full' />
+                                    <p className='mx-auto'>Continue With Facebook</p>
+                                </div>
+                            </Link>
+                            <Link>
+                                <div className='popup-login border px-10 py-3 rounded-xl flex items-center mt-4'>
+                                    <FaGoogle className='w-[37px] h-[30px] text-blue-500 ml-2 rounded-full' />
+                                    <p className='mx-auto'>Continue With Google</p>
+                                </div>
+                            </Link>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
         </div>
     );
 };
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA-tSkQMn3eDlvQhKoTeFB3FL8_XXs3API",
+  authDomain: "simple-firebase-a76e5.firebaseapp.com",
+  projectId: "simple-firebase-a76e5",
+  storageBucket: "simple-firebase-a76e5.appspot.com",
+  messagingSenderId: "650774137877",
+  appId: "1:650774137877:web:a22c1f9506b238522c121c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
