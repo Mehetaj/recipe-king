169d6d9ef06e2ccdf004d7621aedfa9fac7196d5
Mehetaj <121656128+Mehetaj@users.noreply.github.com>
2023-05-02 20:44:35 +0600

    toast added to favorite btn

--- src/Components/Recipe.jsx
+++ src/Components/Recipe.jsx
@@ -1,28 +1,77 @@
 import React from 'react';
+import { Rating } from '@smastrom/react-rating'
+import { FaStar } from 'react-icons/fa'
+import { ToastContainer, toast } from 'react-toastify';
+
+
+
 
 const Recipe = ({ recipe }) => {
     console.log(recipe);
-    const { name, picture, description, ingredients, instructions } = recipe;
+    const { name, picture, description, ingredients, instructions, rating } = recipe;
+
+    const favorite = () => {
+        toast('🦄 Add To Favorite', {
+            position: "top-center",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "light",
+        });
+    }
     return (
         <div className='container w-[1000px] border p-4 mx-auto flex justify-center  mt-10'>
             <div className='w-[1000px] '>
                 <img className=' w-[1000px] h-[600px]' src={picture} alt="" />
                 <h1 className='text-3xl  font-bold font-serif my-4'>{name}</h1>
 
-                <div className='my-6 p-4'>
-                    <h2 className='text-2xl my-4 font-bold'>Ingredients</h2>
-                {
-                    ingredients.map(i => <ol><li className=' list-disc'>{i}</li></ol>)
-                }
+                <div className='flex justify-evenly items-baseline'>
+                    <div className='my-6 p-4 w-[50%]'>
+                        <h2 className='text-2xl my-4 font-bold'>Ingredients</h2>
+                        {
+                            ingredients.map(i => <ol><li className=' list-disc'>{i}</li></ol>)
+                        }
+                    </div>
+
+                    <div className='my-6 w-[50%]'>
+                        <h2 className='text-2xl my-4 font-bold'>Cooking Method</h2>
+                        {
+                            instructions.map(i => <p>{i}</p>)
+                        }
+
+
+                    </div>
+                </div>
+                <hr className='my-4' />
+
+                <div>
+                    <p className='text-red-400 text-center text-xl my-4'>{description}</p>
                 </div>
 
-                <div className='my-6'>
-                    <h2 className='text-2xl my-4 font-bold'>Cooking Method</h2>
-                {
-                    instructions.map(i => <p>{i}</p>)
-                }
+                <div className='flex justify-between items-center'>
+                    <div title={rating} className='flex font-bold items-center' aria-disabled>
+                        <p>{rating}</p>
+                        <Rating isDisabled={true} style={{ maxWidth: 100 }} value={rating} />
+                    </div>
+
+                    <button onClick={favorite} className='text-red-300 text-3xl bg--700 focus:text-red-500'><FaStar /></button>
                 </div>
             </div>
+            <ToastContainer
+                position="top-center"
+                autoClose={5000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="light"
+            />
         </div>
     );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,8 @@ import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import router from './Routes/Routes'
 import AuthProvider from './Provider/AuthProvider'
+import '@smastrom/react-rating/style.css'
+import 'react-toastify/dist/ReactToastify.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
