d530f55811bc64cd40e23b2f59c55d52217195f4
Mehetaj <121656128+Mehetaj@users.noreply.github.com>
2023-05-02 09:06:53 +0600

    author section addded

---
+++ src/Components/FirstSec.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import author from '../assets/author.png';
+
+const FirstSec = () => {
+    return (
+        <div className='h-[300px] mt-20 lg:mt-36 p-4 bg-rose-50 lg:w-[68%] lg:container mx-auto'>
+            <div className='flex justify-evenly border items-center lg:p-20 border-red-300 h-full'>
+                <div className='text-center  font-sans'>
+                <h1 className='text-3xl text-red-400 font-serif'><i>Hello there! I am Maggy. <br /> Start cooking with me.</i></h1>
+                <p className='w-[400px] mt-4 text-gray-500'>
+                Lorem ipsum dolor sit amet, ad consectetur adi picibe elit, sed do eiusmod tempor inci didunt quo labore e dolore magna aliqua ut.
+                </p>
+
+                <button className='underline space-x-4 text-red-400 mt-6'>VIEW MORE</button>
+                </div>
+                <img className='w-[250px] h-[250px]' src={author} alt="" />
+            </div>
+        </div>
+    );
+};
+
+export default FirstSec;
--- src/Components/MainContent.jsx
+++ src/Components/MainContent.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Banner from './Banner';
+import FirstSec from './FirstSec';
 
 
 const MainContent = () => {
     return (
         <div>
             <Banner />
+            <FirstSec />
         </div>
     );
 };
